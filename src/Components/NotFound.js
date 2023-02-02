db494598a2c3d77c30272f2cc2048e0a21352fe9
Pipetboy <felipefelizx@gmail.com>
2023-02-02 14:43:08 -0300

    Fix: Update 404 - PAGINA NO ENCONTRADA

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div id="notfound">
+      <div class="notfound">
+        <div class="notfound-404">
+          <h1>Oops!</h1>
+        </div>
+        <h2>404 - Pagina no encontrada</h2>
+        <p>Es posible que la página que está buscando haya sido eliminada debido a un cambio de nombre o no esté disponible temporalmente.</p>
+        <Button href="/">
+          Volver al inicio
+        </Button>
+      </div>
     </div>
   );
 };
