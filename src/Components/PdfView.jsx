76e118fee889788aaaf476d3e587d3e1411d7379
Pipetboy <felipefelizx@gmail.com>
2023-02-01 21:52:01 -0300

    Feature: Add pdf viewer

---
+++ src/Components/PdfView.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PdfView({ src }) {
+  return <iframe src={src} />;
+}
+
+export default PdfView;
--- src/Components/Unidad 1/Guias.jsx
+++ src/Components/Unidad 1/Guias.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 
+/*Pdf viewer */
+import PdfViewer from "../PdfView";
+
+
 function ControlledTabsExample() {
   const [key, setKey] = useState("home");
 
@@ -14,12 +18,16 @@ function ControlledTabsExample() {
     >
       <Tab eventKey="home" title="Home">
         <h1>Hola página 1</h1>
+
+        <PdfViewer src="https://eqpro.es/wp-content/uploads/2018/11/Ejemplo.pdf" />
       </Tab>
       <Tab eventKey="profile" title="Profile">
         <h1>Hola página 2</h1>
+        <PdfViewer src="https://eqpro.es/wp-content/uploads/2018/11/Ejemplo.pdf" />
       </Tab>
       <Tab eventKey="contact" title="Contact">
         <h1>Hola página 3</h1>
+        <PdfViewer src="https://eqpro.es/wp-content/uploads/2018/11/Ejemplo.pdf" />
       </Tab>
     </Tabs>
   );
