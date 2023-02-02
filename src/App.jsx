3e4aa46651cc01304963713f3d489803fdccdc82
Pipetboy <felipefelizx@gmail.com>
2023-02-02 00:00:55 -0300

    Fix: after ordering routes, put back which is which

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,13 @@ import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import About from "./Components/About";
 
+/**/
+
 /*Unidad 1*/
+import Learning1 from "./Components/Unidades/Unidad 1/Aprendisaje";
+import Guiades1 from "./Components/Unidades/Unidad 1/Guias";
+import Exam1 from "./Components/Unidades/Unidad 1/Pruebas";
 
-import Learning1 from "./Components/Unidad 1/Aprendisaje";
-import Guiades1 from "./Components/Unidad 1/Guias";
-import Exam1 from "./Components/Unidad 1/Pruebas";
-
-
-/*Unidad 2*/
-/*
-import Learning2 from "./Components/Unidad 2/Aprendisaje";
-import Guiades2 from "./Components/Unidad 2/Guias";
-import Exam2 from "./Components/Unidad 2/Pruebas";
-*/
-/*Unidad 3*/
-/*
-import Learning3 from "./Components/Unidad 3/Aprendisaje";
-import Guiades3 from "./Components/Unidad 3/Guias";
-import Exam3 from "./Components/Unidad 3/Pruebas";
-*/
 
 function App() {
   return (
