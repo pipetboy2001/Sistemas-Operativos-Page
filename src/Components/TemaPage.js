2303d748b3f15b1981e848cb4fef364e6b0d993b
Pipetboy <felipefelizx@gmail.com>
2023-03-19 20:16:13 -0300

    Feature: TemaPage for imagen

--- src/Components/TemaPage.js
+++ src/Components/TemaPage.js
@@ -1,9 +1,32 @@
+import React from "react";
 import { useParams } from "react-router-dom";
+import imagenes from "../Json/Tema.json";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function TemaPage() {
-    let { temaId } = useParams();
+    const { temaId } = useParams();
 
-    return <h1>Hola mundo, este es el tema {temaId}</h1>;
+    return (
+        <div className="container">
+            <h1>{temaId}: </h1>
+
+            {imagenes.map((imagen, index) => (
+                <div className="row" key={index}>
+                    <div className="col-md-8">
+                        <img
+                            className="d-block w-100"
+                            src={imagen.imagen}
+                            alt={imagen.nombre}
+                        />
+                    </div>
+                    <div className="col-md-4">
+                        <h3>{imagen.nombre}</h3>
+                        <p>{imagen.descripcion}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default TemaPage;
