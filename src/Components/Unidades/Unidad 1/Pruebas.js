abcc3b3f7af095d2316cc590ba7fc5722b70a1c1
Felipe Fernandez <felipefelizx@gmail.com>
2023-05-22 22:35:22 -0400

    Fix: Add past test

--- src/Components/Unidades/Unidad 1/Pruebas.js
+++ src/Components/Unidades/Unidad 1/Pruebas.js
@@ -11,7 +11,7 @@ import List from "./../../PreguntaPorPrueba";
 const pruebas = [
   { id: "1", title: "Global 2-2022", pdfUrl: "https://drive.google.com/file/d/1Oeis1VPzwr5ycNPDwf1jg2B_ctwFKwOU/preview", unidad: "Unidad 1", prueba: "Global 2-2022" },
   { id: "2", title: "Acosta 1-2022", pdfUrl: "https://drive.google.com/file/d/1O_03T2spdcED574ukmkheqU3EUI-Pbsu/preview", unidad: "Unidad 1", prueba: "Acosta 1-2022" },
-  { id: "3", title: "Rannou 1-2022", pdfUrl: "", unidad: "Unidad 1", prueba: "Rannou 1-2022" }
+  { id: "3", title: "Rannou 1-2022", pdfUrl: "https://drive.google.com/file/d/1OaF_KJAuJ2GMH_yYE_pbubnz5zP4Ttwq/preview", unidad: "Unidad 1", prueba: "Rannou 1-2022" }
 ];
 
 function Prueba() {
