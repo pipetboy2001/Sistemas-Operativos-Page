34da0090b74fbb4a637c35306d8134a0476a681a
Felipe Fernandez <felipefelizx@gmail.com>
2023-07-31 00:56:55 -0400

    Feature - Themes now from PDF

--- src/Components/TemaPage.js
+++ src/Components/TemaPage.js
@@ -1,35 +1,46 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import imagenes from "../Json/Tema.json";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Accordion from "react-bootstrap/Accordion";
+import { ImEye } from "react-icons/im";
+import PdfViewer from "./PdfView";
+
+// Importar los datos desde los archivos JSON
+import temas from './../Json/TemasResumenes.json';
+import resumenes from './../Json/Resumenes.json';
 
 function TemaPage() {
-    const { temaId } = useParams();
-    // Filtrar las imágenes por temaId
-    const imagenesFiltradas = imagenes.filter((imagen) => imagen.temaId === temaId);
+  const { temaId } = useParams();
+  const resumenesPorTema = resumenes.filter((resumen) => resumen.tema === temaId);
+  const temaSeleccionado = temas.find((tema) => tema.id === temaId);
 
+  return (
+    <div className="container">
+      <center><h1>{temaSeleccionado ? temaSeleccionado.nombre : ""}</h1></center>
 
-    return (
-        <div className="container">
-            <h1>{temaId}: </h1>
-
-            {imagenesFiltradas.map((imagen, index) => (
-                <div className="row" key={index}>
-                    <div className="col-md-8">
-                        <img
-                            className="d-block w-100"
-                            src={imagen.imagen}
-                            alt={imagen.nombre}
-                        />
-                    </div>
-                    <div className="col-md-4">
-                        <h3>{imagen.nombre}</h3>
-                        <p>{imagen.descripcion}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+      {resumenesPorTema.length > 0 ? (
+        <Tabs className="mx-auto w-75" defaultActiveKey={resumenesPorTema[0].nombre} id="resumenes">
+          {resumenesPorTema.map((resumen) => (
+            <Tab key={resumen.id} eventKey={resumen.nombre} title={resumen.nombre}>
+              <Accordion className="mx-auto w-75" defaultActiveKey={resumen.nombre}>
+                <Accordion.Item eventKey={resumen.nombre}>
+                  <Accordion.Header>
+                    Ver Pdf <ImEye />
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <PdfViewer src={resumen.link} />
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Tab>
+          ))}
+        </Tabs>
+      ) : (
+        <p>No hay resúmenes para este tema</p>
+      )}
+    </div>
+  );
 }
 
 export default TemaPage;
